b85cac6387d7d90700a3a8d040e089299aab3232
Felipeszzz <160777929+Felipeszzz@users.noreply.github.com>
2024-05-21 22:08:06 +0000

    Exercicio 4

--- .ccls-cache/@home@runner@ListaExercicio/main.c
+++ .ccls-cache/@home@runner@ListaExercicio/main.c
@@ -1,56 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-struct Produto {
-    char nome[50];
-    float preco;
-    int quantidade;
-};
-
-
-float calcularTotal(struct Produto produtos[], int numProdutos) {
-    float total = 0;
-    for (int i = 0; i < numProdutos; i++) {
-        total += produtos[i].preco * produtos[i].quantidade;
-    }
-    return total;
-}
-
 int main() {
-    int numProdutos;
-    printf("Quantos produtos deseja cadastrar? ");
-    scanf("%d", &numProdutos);
+    int linhas1, colunas1, linhas2, colunas2;
 
-   
-    struct Produto *produtos = (struct Produto *)malloc(numProdutos * sizeof(struct Produto));
 
-    
-    for (int i = 0; i < numProdutos; i++) {
-        printf("\nProduto %d:\n", i + 1);
-        printf("Nome: ");
-        scanf("%s", produtos[i].nome);
-        printf("Preço: ");
-        scanf("%f", &produtos[i].preco);
-        printf("Quantidade em estoque: ");
-        scanf("%d", &produtos[i].quantidade);
+    printf("Digite o número de linhas da matriz 1: ");
+    scanf("%d", &linhas1);
+    printf("Digite o número de colunas da matriz 1: ");
+    scanf("%d", &colunas1);
+    printf("Digite o número de linhas da matriz 2: ");
+    scanf("%d", &linhas2);
+    printf("Digite o número de colunas da matriz 2: ");
+    scanf("%d", &colunas2);
+
+
+    if (colunas1 != linhas2) {
+        printf("Erro: O número de colunas da matriz 1 deve ser igual ao número de linhas da matriz 2.\n");
+        return 1;
     }
 
-    
-    printf("\nInformações dos Produtos:\n");
-    for (int i = 0; i < numProdutos; i++) {
-        printf("Produto %d:\n", i + 1);
-        printf("Nome: %s\n", produtos[i].nome);
-        printf("Preço: %.2f\n", produtos[i].preco);
-        printf("Quantidade em estoque: %d\n", produtos[i].quantidade);
+
+    int **matriz1 = (int **)malloc(linhas1 * sizeof(int *));
+    int **matriz2 = (int **)malloc(linhas2 * sizeof(int *));
+    for (int i = 0; i < linhas1; i++) {
+        matriz1[i] = (int *)malloc(colunas1 * sizeof(int));
+    }
+    for (int i = 0; i < linhas2; i++) {
+        matriz2[i] = (int *)malloc(colunas2 * sizeof(int));
     }
 
-    
-    float total = calcularTotal(produtos, numProdutos);
-    printf("\nValor total em estoque: %.2f\n", total);
 
-    
-    free(produtos);
+    printf("Digite os elementos da matriz 1:\n");
+    for (int i = 0; i < linhas1; i++) {
+        for (int j = 0; j < colunas1; j++) {
+            scanf("%d", &matriz1[i][j]);
+        }
+    }
+
+    printf("Digite os elementos da matriz 2:\n");
+    for (int i = 0; i < linhas2; i++) {
+        for (int j = 0; j < colunas2; j++) {
+            scanf("%d", &matriz2[i][j]);
+        }
+    }
+
+
+    int **resultado = (int **)malloc(linhas1 * sizeof(int *));
+    for (int i = 0; i < linhas1; i++) {
+        resultado[i] = (int *)malloc(colunas2 * sizeof(int));
+        for (int j = 0; j < colunas2; j++) {
+            resultado[i][j] = 0;
+            for (int k = 0; k < colunas1; k++) {
+                resultado[i][j] += matriz1[i][k] * matriz2[k][j];
+            }
+        }
+    }
+
+
+    printf("Resultado da multiplicação das matrizes:\n");
+    for (int i = 0; i < linhas1; i++) {
+        for (int j = 0; j < colunas2; j++) {
+            printf("%d\t", resultado[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    for (int i = 0; i < linhas1; i++) {
+        free(matriz1[i]);
+    }
+    for (int i = 0; i < linhas2; i++) {
+        free(matriz2[i]);
+    }
+    for (int i = 0; i < linhas1; i++) {
+        free(resultado[i]);
+    }
+    free(matriz1);
+    free(matriz2);
+    free(resultado);
 
     return 0;
 }
